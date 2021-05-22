76d3f89f757eda582ed3f17228f40977093ed610
yuratouch <yu.touch@icloud.com>
2021-05-22 22:17:32 +0300

    Delete redundant .join methods in CreateHeroes method

--- js/script.js
+++ js/script.js
@@ -73,13 +73,10 @@ class Heroes {
             .join("");
         let heroesaddAccordionClick = heroes
             .map(hero => hero.addAccordionClick())
-            .join("");
         let heroesAttribute = heroes
             .map(hero => hero.renderAttribute())
-            .join("");
         let heroesaddAttributeHover = heroes
             .map(hero => hero.addAttributeHover())
-            .join("");
         ACCORDION.innerHTML = heroesAccordion
     }
 }
